ecc93ec23c8ea023d02ba90c1737677154e858df
shrfrn <shrfrn@gmail.com>
2023-07-03 17:48:04 +0300

    Add support for datalist in textbox cmp

--- js/cmps/text-box.cmp.js
+++ js/cmps/text-box.cmp.js
@@ -2,15 +2,19 @@ export default {
     props: ['info'],
     template: `
         <section>
+            <datalist :id="datalistId">
+                <option v-for="opt in info.opts" :value="opt" :key="opt" />
+            </datalist>
             <label>
                 {{info.label}}
-                <input type="text" v-model="val" @change="reportVal" />
+                <input type="text" v-model="val" @change="reportVal" :list="datalistId"/>
             </label>  
         </section>
     `,
     data() {
         return {
             val: '',
+            datalistId: _makeId()
         }
     },
     methods: {
@@ -18,4 +22,15 @@ export default {
             this.$emit('set-val', this.val)
         },
     },
+}
+
+function _makeId(length = 6) {
+    var txt = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return txt
 }
